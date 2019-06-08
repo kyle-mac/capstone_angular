d110afd5b8120d0adc3ec270314347c3d9f21630
Kyle MacIlvain <kyle.macilvain@berkeley.edu>
2019-06-08 19:13:50 -0400

    UI Cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
 import { RecommendationsComponent } from './recommendations/recommendations.component';
 import { RouterModule, Routes} from '@angular/router';
-import { SearchComponent } from './search/search.component';
 import { HomeComponent } from './home/home.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
     RecommendationsComponent,
-    SearchComponent,
-    HomeComponent
+    HomeComponent,
+    SearchResultsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
       {path:'home', component: HomeComponent},
-      {path:'about', component: AboutComponent},
       {path:'recommendations',component: RecommendationsComponent},
-      {path:'search',component: SearchComponent}
+      {path:'searchResults',component: SearchResultsComponent}
+
     ])
   ],
   providers: [],
