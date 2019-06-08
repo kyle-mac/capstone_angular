1f179b7319acefa79e1f298742681dce6423ff89
Kyle MacIlvain <kyle.macilvain@berkeley.edu>
2019-06-08 17:50:30 -0400

    Search updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,25 @@ import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { RecommendationsComponent } from './recommendations/recommendations.component';
 import { RouterModule, Routes} from '@angular/router';
+import { SearchComponent } from './search/search.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    RecommendationsComponent
+    RecommendationsComponent,
+    SearchComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
+      {path:'home', component: HomeComponent},
       {path:'about', component: AboutComponent},
-      {path:'recommendations',component: RecommendationsComponent}
+      {path:'recommendations',component: RecommendationsComponent},
+      {path:'search',component: SearchComponent}
     ])
   ],
   providers: [],
