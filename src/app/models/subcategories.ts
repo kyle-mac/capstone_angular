1d1f06348b6dae90599cfb94a1c8aa1322688387
Kyle MacIlvain <kyle.macilvain@berkeley.edu>
2019-07-06 14:32:26 -0400

    Updated subcategories

--- src/app/models/subcategories.ts
+++ src/app/models/subcategories.ts
@@ -3,7 +3,21 @@ import { Subcategory } from './subcategory';
 
 
 export const SUBCATEGORIES: Subcategory[] = [
-{ name: 'Sports & Outdoors' },
-{ name: 'Other Sports' },
-{ name: 'Dance' }
+{ name: 'Baby & Toddler Toys' },
+{ name: 'Fan Shop' },
+{ name: 'Tricycles, Scooters & Wagons' },
+{ name: 'Electronics for Kids' },
+{ name: 'Dress Up & Pretend Play' },
+{ name: 'Grown-Up Toys' },
+{ name: 'Novelty & Gag Toys' },
+{ name: 'Learning & Education' },
+{ name: 'Puzzles' },
+{ name: 'Sports & Outdoors Play' },
+{ name: 'Arts & Crafts' },
+{ name: 'Pary Supplies' },
+{ name: 'Toy Remote Control & Play Vehicles' },
+{ name: 'Dolls & Accessories' },
+{ name: 'Hobbies' },
+{ name: 'Stuffed Animals & Plush' },
+{ name: 'Games' },
 ];
