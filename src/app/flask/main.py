45014048d9d359f70703c3ef1639b0eef8878635
Kyle MacIlvain <kyle.macilvain@berkeley.edu>
2019-07-06 19:51:52 -0400

    Fixed component return

--- src/app/flask/main.py
+++ src/app/flask/main.py
@@ -26,5 +26,27 @@ def get_recommendations(category):
     print(recommendations)
     return jsonify(recommendations)
 
+@app.route('/features/<asin>', methods=['GET'])
+
+def get_features(asin):
+
+    get_query = "SELECT * FROM product_features WHERE asin = '{}'.format(asin)
+    recommendations = query_db(get_query,'GET')
+    print(recommendations)
+    return jsonify(recommendations)
+
+@app.route('/toyrecommendations/<subcategory>', methods=['GET'])
+
+def get_recommended_toys():
+
+    #subcategory logic TBD
+    get_query = "SELECT meta_Toys_and_Games.*, product_features.top_feature_exemplar
+                 FROM meta_Toys_and_Games
+                 INNER JOIN product_features ON meta_Toys_and_Games.asin = product_features.asin"
+    recommendations = query_db(get_query,'GET')
+    print(recommendations)
+    return jsonify(recommendations)
+
+
 if __name__ == '__main__':
      app.run()
