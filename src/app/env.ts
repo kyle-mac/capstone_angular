116c8a3ea5ca0d6ff35ee751c70e017a4e0ec8a8
EC2 Default User <ec2-user@ip-172-31-93-207.ec2.internal>
2019-07-17 19:37:46 +0000

    Submitting for server branch

--- src/app/env.ts
+++ src/app/env.ts
@@ -1,1 +1,1 @@
-export const API_URL = 'http://localhost:5000';
+export const API_URL = 'http://ec2-34-201-54-41.compute-1.amazonaws.com:5000';
--- src/app/flask/main.py
+++ src/app/flask/main.py
@@ -49,4 +49,4 @@ def get_recommended_toys():
 
 
 if __name__ == '__main__':
-     app.run()
+     app.run(host='0.0.0.0')
