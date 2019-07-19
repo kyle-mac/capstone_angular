f0e31c5fb1d9167858682503f41975ac9373e94c
EC2 Default User <ec2-user@ip-172-31-93-207.ec2.internal>
2019-07-19 03:25:34 +0000

    Changed logic

--- src/app/flask/recommendLogic/Recommendation_Logic.py
+++ src/app/flask/recommendLogic/Recommendation_Logic.py
@@ -1,36 +1,35 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# ## Code to read the files
-
 # In[1]:
 
 
-def return_recommendations(featureList, productList):
-    #example productList = ['chatter telephone', 'radio flyer wagon']
-    #example featureList = ['minute', 'son', 'house', 'trike']
-
-    #return a list of ASIN values
-    recommendedItems = [B00,B01,B02,B03]
-
-    return recommendedItems
-
-
 import pandas as pd
-import numpy as np
 import gzip
 import time
 import os, sys, time
 import collections
 from collections import Counter
-import json
 import random
+import itertools
+import numpy as np
+import json
 
 
 # In[2]:
 
 
-#Read product metadata
+#Read the MF features
+with open('neighbors5.json', 'r') as f_:
+     data = json.load(f_)
+#Convert to a pandas df with item and similar items
+df = pd.DataFrame(data.items(), columns=['item', 'similar'])
+
+
+# In[3]:
+
+
+#Function to read product metadata
 def parse(path):
   g = gzip.open(path, 'rb')
   for l in g:
@@ -44,200 +43,163 @@ def getDF(path):
     i += 1
   return pd.DataFrame.from_dict(df, orient='index')
 
-dfMeta = getDF('meta_Toys_and_Games.json.gz')
-#Pull out the subcategory for the products from the categories field.
-dfMeta['subCateg'] = dfMeta['categories'].map(lambda x:x[0][1] if len(x[0]) > 1 else x[0][0])
-
 
 # In[4]:
 
 
-#Read text based product features
-dfTextF = pd.read_csv('product_features.tar.gz',compression = 'gzip')
-dfTextF.rename(columns = {'product_features.0.csv':'asin'," 'note'":'feature'}, inplace = True)
+#Read the Metadata
+dfMeta = getDF('meta_Toys_and_Games.json.gz')
+dfMeta['subCateg'] = dfMeta['categories'].map(lambda x:x[0][1] if len(x[0]) > 1 else x[0][0])
 
 
 # In[5]:
 
 
-#Read title and description based features
-product_attribute_features_filepath = './product_attribute_features.harvested.0.csv'
-with open('product_attribute_features.harvested.0.csv', mode='r') as in_f:
-   productTitleDescFeatures = pd.read_csv(in_f, names=['asin','overall','feature'])
-   print('File {} contains {} total product features.'.format(product_attribute_features_filepath,
-                                                              len(productTitleDescFeatures)))
-   print(productTitleDescFeatures[:10])
+#Read the text features based on reviews
+dfTextF = pd.read_csv('product_features.tar.gz',compression = 'gzip')
+dfTextF.rename(columns = {'product_features.0.csv':'asin'," 'note'":'feature'}, inplace = True)
 
 
 # In[6]:
 
 
-#Read MF based similar products
-with open('neighbors5.json', 'r') as f_:
-     data = json.load(f_)
-df = pd.DataFrame(data.items(), columns=['item', 'similar'])
+#Read the text features based on title and description
+product_attribute_features_filepath = './product_attribute_features.harvested.0.csv'
+with open('product_attribute_features.harvested.0.csv', mode='r') as in_f:
+   productTitleDescFeatures = pd.read_csv(in_f, names=['asin','overall','feature'])
 
 
 # In[7]:
 
 
-#Code to get the recommended items given a list of user items asins, based on text features of the items.
-def findSimilarsText(userItems):
-
+def findSimilarsText(userItems): 
+    
     userCategories = np.unique(dfMeta[dfMeta['asin'].isin(userItems)]['subCateg'].reset_index(drop = True))
-    print(userCategories)
-
+#     print(userCategories)
+    
     similarItems = []
-
+    
     #Find all items with at least one feature in common with the user selected item features
     for item in userItems:
         reviewFeatures = dfTextF[dfTextF['asin'] == item]['feature'].to_list()
 #         print(reviewFeatures)
         for feature in reviewFeatures:
             simProds = dfTextF[dfTextF['feature'] == feature]['asin'].to_list()
-            print(feature,len(simProds))
+#             print(feature,len(simProds))
             for prod in simProds:
                 if prod != item:
                     similarItems.append(prod)
 
 
         titleDescFeatures = productTitleDescFeatures[productTitleDescFeatures['asin'] == item]['feature'].to_list()
+#         print(titleDescFeatures)
         for feature in titleDescFeatures:
             simProds = productTitleDescFeatures[productTitleDescFeatures['feature'] == feature]['asin'].to_list()
-            print(feature,len(simProds))
+#             print(feature,len(simProds))
             for prod in simProds:
                 if prod != item:
                     similarItems.append(prod)
-
+                
     temp = dfMeta[dfMeta['asin'].isin(similarItems)]
-    print('subCategories',Counter(temp['subCateg']))
+#     print('subCategories',Counter(temp['subCateg']))
     selectSimilarItems = temp[temp['subCateg'].isin(userCategories)]['asin'].reset_index(drop = True)
     a = Counter(selectSimilarItems) #Items limited to same subcategory as user picked items
     b = Counter(similarItems) #All similar items irrespective of category
-    print('Number of items before and after limiting categories',len(b),len(a))
-
+#     print('Number of items before and after limiting categories',len(b),len(a))
+    
     recommendedItems = []
     for asin,count in a.most_common(4):
         recommendedItems.append(asin)
     for asin,count in b.most_common(2):
         recommendedItems.append(asin)
-
+    
     return recommendedItems
 
 
-# In[10]:
+# In[8]:
 
 
-#Sample code to call the function
-
-#Picking a couple of random items. In actual engine, the user items would be passed into the function call and this would not be needed
-
-numSelections = 2
-prodList = dfTextF.groupby('asin').agg('count').index.to_list()
-userItems = random.sample(prodList, numSelections)
-print(userItems)
-print("""""")
-
-#Sample function call
-simItemsList = findSimilarsText(userItems)
-simItemsList
-
-
-# In[16]:
-
-
-#Function to create recommendations based on MF-based similarity to all items in userItems (list of asins picked by user)
-
+#Function to read the similar items to all items in userItems (list of asins picked by user, and output the most common asins within those)
 def findSimilarsMF(userItems):
     userCategories = np.unique(dfMeta[dfMeta['asin'].isin(userItems)]['subCateg'].reset_index(drop = True))
     similarItems = []
-
+    
     similarItemsDf = df[df['item'].isin(userItems)]['similar'].reset_index(drop = True)
 #     similarItems = [item for sublist in similarItemsDf for item in sublist]
     for i in range(similarItemsDf.shape[0]):
         for item in similarItemsDf[i]:
             similarItems.append(item)
 #     print(len(similarItems),similarItems)
-
+    
     for uItem in userItems:
         similarItems = [x for x in similarItems if x != uItem]
-
+        
     temp = dfMeta[dfMeta['asin'].isin(similarItems)]
 #     print('subCategories',Counter(temp['subCateg']))
     selectSimilarItems = temp[temp['subCateg'].isin(userCategories)]['asin'].reset_index(drop = True)
-
+    
     a = Counter(selectSimilarItems) #Items limited to same subcategory as user picked items
     b = Counter(similarItems) #All similar items irrespective of category
-
+    
     recommendedItems = []
     for asin,count in a.most_common(4):
         recommendedItems.append(asin)
     for asin,count in b.most_common(2):
         recommendedItems.append(asin)
-
+    
     return recommendedItems
 
 
-# In[20]:
+# In[9]:
 
 
-#Sample function call to get MF based recommendations
-simItemsListMF = findSimilarsMF(userItems)
-simItemsListMF
+def findSimilarsTextOnly(features): 
+   
+    similarItems = []
+    
+    #Find all items with at least one feature in common with the user selected text features
+    for feature in features:
+        simProds = dfTextF[dfTextF['feature'] == feature]['asin'].to_list()
+        print(feature,len(simProds))
+        for prod in simProds:
+            similarItems.append(prod)
 
 
-# ### Code to show the output of recommendations
-
-# In[25]:
-
-
-import matplotlib.pyplot as plt
-get_ipython().run_line_magic('matplotlib', 'inline')
-from PIL import Image
-import requests
-from io import BytesIO
-
-def showImages(images):
-    imageswidth = len(images)*4
-    fig=plt.figure(figsize=(imageswidth, 8))
-
-    for i in range(0,len(images)):
-        response = requests.get(images[i])
-        img = Image.open(BytesIO(response.content))
-        fig.add_subplot(1, len(images), i+1)
-        plt.imshow(img)
+    for feature in features:
+        simProds = productTitleDescFeatures[productTitleDescFeatures['feature'] == feature]['asin'].to_list()
+        print(feature,len(simProds))
+        for prod in simProds:
+            similarItems.append(prod)
+                
+    b = Counter(similarItems)
+    
+    recommendedItems = []
+    for asin,count in b.most_common(6):
+        recommendedItems.append(asin)
+        
+    return recommendedItems
 
 
-# In[27]:
+# In[37]:
 
 
-#Pick some user items randomly to find similar products for
-numSelections = 2 #This can be varied based on how long we want the user list to be.
-prodList = dfTextF.groupby('asin').agg('count').index.to_list()
-userItems = random.sample(prodList, numSelections)
-
-#Show user items picked
-images = dfMeta[dfMeta['asin'].isin(userItems)]['imUrl'].to_list()
-showImages(images)
-dfMeta[dfMeta['asin'].isin(userItems)]
-
-
-# In[26]:
-
-
-#Find and display similar items using text features
-simItemsList = findSimilarsText(userItems)
-images = dfMeta[dfMeta['asin'].isin(simItemsList)]['imUrl'].to_list()
-showImages(images)
-dfMeta[dfMeta['asin'].isin(simItemsList)]
+def return_recommendations(featureList,productList):
+    productAsins = dfMeta[dfMeta['title'].isin(productList)]['asin'].to_list()
+#     print(productAsins)
+    recommendedItems = {}
+    textProds = findSimilarsText(productAsins)
+    MFProds = findSimilarsMF(productAsins)
+    featureProds = findSimilarsTextOnly(featureList)
+    recommendedItems['textProds'] = textProds
+    recommendedItems['MFProds'] = MFProds
+    recommendedItems['featureProds'] = featureProds
+    return recommendedItems
 
 
-# In[28]:
+# In[39]:
 
 
-#Find and display similar items using MF
-simItemsListMF = findSimilarsMF(userItems)
-images = dfMeta[dfMeta['asin'].isin(simItemsListMF)]['imUrl'].to_list()
-showImages(images)
-dfMeta[dfMeta['asin'].isin(simItemsListMF)]
+# sample test case
+# return_recommendations([' chalkboard',' everyone'],productList)
+
 
