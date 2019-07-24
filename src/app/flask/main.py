e3a50f3ca47858defe6eaadefd6bde42933618b1
Kyle MacIlvain <kyle.macilvain@berkeley.edu>
2019-07-23 20:57:32 -0400

    Changes to flask

--- src/app/flask/main.py
+++ src/app/flask/main.py
@@ -118,4 +118,4 @@ def get_final_result(finalProductsList):
     return jsonify(recommendations)
 
 if __name__ == '__main__':
-     app.run(host='0.0.0.0', debug=True)
+     app.run(host='0.0.0.0', debug=True, use_reloader=False)
