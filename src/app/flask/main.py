6ed79faa58c809cacba8068b70a2ddb7e90a16fb
Kyle MacIlvain <kyle.macilvain@berkeley.edu>
2019-07-28 23:53:29 -0400

    Fixed logging

--- src/app/flask/main.py
+++ src/app/flask/main.py
@@ -55,7 +55,6 @@ def get_recommended_toys(keywords, category):
     logging.basicConfig(filename='myapp.log', level=logging.INFO)
     logging.info('Subcategory is {}'.format(category))
 
-    logging.info(df.head())
 
     stopwords = ["i","me","my","myself","we","our","ours","ourselves","you","your","yours","yourself","yourselves","he","him","his","himself","she","her","hers","herself","it","its","itself","they","them","their","theirs","themselves","what","which","who","whom","this","that","these","those","am","is","are","was","were","be","been","being","have","has","had","having","do","does","did","doing","a","an","the","and","but","if","or","because","as","until","while","of","at","by","for","with","about","against","between","into","through","during","before","after","above","below","to","from","up","down","in","out","on","off","over","under","again","further","then","once","here","there","when","where","why","how","all","any","both","each","few","more","most","other","some","such","no","nor","not","only","own","same","so","than","too","very","can","will","just","don","should","now"]
 
@@ -83,7 +82,7 @@ def get_recommended_toys(keywords, category):
     logging.info('Get query is {}'.format(get_query))
 
     recommendations = query_db(get_query,'GET')
-    logging.info('Recommendations are {}'.format(recommendations))
+    #logging.info('Recommendations are {}'.format(recommendations))
     return jsonify(recommendations)
 
 
@@ -117,6 +116,7 @@ def get_final_result(finalProductsList):
 
 
     get_query = "SELECT * FROM meta_and_features_final WHERE asin IN ({})".format(productString)
+    logging.info("Final query is {}".format(get_query))
     recommendations = query_db(get_query,'GET')
     return jsonify(recommendations)
 
